7705e2144cf8f965502fb45501b24dd0b8468ef4
anon <>
2025-04-17 18:50:20 +0200

    add search

--- src/routes/leagues.index.tsx
+++ src/routes/leagues.index.tsx
@@ -1,36 +1,49 @@
 import { createFileRoute, Link, Outlet } from '@tanstack/react-router'
 import { useGetLeaguesQuery } from '../services/volleyApi'
+import { useState } from 'react'
+import { useDebounce } from '@uidotdev/usehooks'
 
 export const Route = createFileRoute('/leagues/')({
   component: RouteComponent,
 })
 
+const MIN_SEARCH_LENGTH = 3
+
 function RouteComponent() {
-  const { isLoading, isError, data } = useGetLeaguesQuery({
-    'x-rapidapi-key': import.meta.env.VITE_VOLLEY_API_X_RAPID_API_KEY,
-    search: 'Italia',
-  })
+  const [searchTerm, setSearchTerm] = useState('')
+  const debouncedSearchTerm = useDebounce(searchTerm, 300)
 
-  if (isLoading) {
-    return '...Loading'
-  }
-
-  if (isError) {
-    return 'Error'
-  }
+  const { isFetching, isError, isSuccess, data } = useGetLeaguesQuery(
+    {
+      'x-rapidapi-key': import.meta.env.VITE_VOLLEY_API_X_RAPID_API_KEY,
+      search: debouncedSearchTerm,
+    },
+    { skip: debouncedSearchTerm.length < MIN_SEARCH_LENGTH },
+  )
 
   return (
-    <ul>
-      {data?.response.map((league) => (
-        <li key={league.id}>
-          <Link
-            to={`/leagues/$leagueId`}
-            params={{ leagueId: league.id.toString() }}
-          >
-            {league.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <label>Search: </label>
+      <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      ></input>
+      {isError && <div>Error</div>}
+      {isFetching && <div>Loading...</div>}
+      {isSuccess && (
+        <ul>
+          {data?.response.map((league) => (
+            <li key={league.id}>
+              <Link
+                to={`/leagues/$leagueId`}
+                params={{ leagueId: league.id.toString() }}
+              >
+                {league.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
